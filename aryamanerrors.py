d63b9174fb43d2d3a52bfcaa505f2414e917a54b
HarshilMital <harshil.mital20@gmail.com>
2022-07-02 16:52:21 +0530

    starter code for error files

---
+++ aryamanerrors.py
@@ -0,0 +1,15 @@
+#d e i
+
+def errors():
+    inp_file = open('Run.txt', 'r')
+    lines = inp_file.readlines()
+    for i in range(len(lines)):
+        i.rstrip('\n')
+
+    lines = [i.split() for i in lines]
+
+    #d
+
+    #e
+
+    #i
---
+++ ieshaanerrors.py
@@ -0,0 +1,15 @@
+#b c f
+
+def errors():
+    inp_file = open('Run.txt', 'r')
+    lines = inp_file.readlines()
+    for i in range(len(lines)):
+        i.rstrip('\n')
+
+    lines = [i.split() for i in lines]
+
+    #b
+
+    #c
+
+    #f
