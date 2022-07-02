10fc63e7ede438ba868416e9a052186ea27ba9cb
Aryaman <someoneg4@gmail.com>
2022-07-02 17:39:04 +0530

    d done

--- aryamanerrors.py
+++ aryamanerrors.py
@@ -1,3 +1,4 @@
+import CO_Assignment
 #d e i
 
 def errors():
@@ -7,9 +8,23 @@ def errors():
         i.rstrip('\n')
 
     lines = [i.split() for i in lines]
+    errorList = [None for i in range(256)]
 
+    
     #d
+    #Illegal use of FLAGS register
+    for i in range(len(lines)):
+        if 'FLAGS' in lines[i]:
+            curLine = lines[i]
+            if (curLine[0][-1] == ':'):
+                #if label is present at beginning of instruction
+                curLine = curLine[1:]
+            if curLine[0] != 'mov':
+                if errorList[i] == None:
+                    errorList[i] = 'd'
 
     #e
+    #Illegal Immediate values (more than 8 bits)
+
 
     #i
