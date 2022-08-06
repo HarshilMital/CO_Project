1e0f9be34221b811dc5335c9d5812440fbd1ec6c
Aryaman <someoneg4@gmail.com>
2022-08-06 19:22:05 +0530

    type a exec

--- Simulator.py
+++ Simulator.py
@@ -8,8 +8,10 @@ PC = 0
 def pcPrinter (PC, base):
     '''returns binary value of program counter'''
     '''also can do binary conversion w extra zero bits appended for a given base length'''
-    binVal = bin(int(PC))[2:]
     return((base-len(binVal))*'0' + binVal)
+def binToDec(n):
+    return int(n, 2)
+
 
 #initialise register file
 RF = {}
@@ -22,6 +24,7 @@ def keyReturn(d, s):
         if s in d[i]:
             return i
 
+
 def executionEngine(instruction):
     types = {'A':['10000', '10001', '10110', '11010', '11011', '11100'], 'B':['10010', '11000', '11001'], 'C':['10011', '10111', '11101', '11110'], 'D':['10100', '10101'], 'E':['11111', '01100', '01101', '01111'], 'F':['01010']}
     opcode = instruction[:5]
@@ -31,11 +34,60 @@ def executionEngine(instruction):
         r1 = instruction[7:10]
         r2 = instruction[10:13]
         r3 = instruction[13:]
+
+        if opcode == '10000':
+            #addition
+            summ = binToDec(RF[r1]) + binToDec(RF[r2])
+            if len(pcPrinter(summ, 0))>16:
+                #overflow
+                temp = list(RF['111'])
+                temp[12] = 1
+                RF['111'] = ''.join(temp)
+            else:
+                RF[r3] = pcPrinter(summ, 16)
         
+        if opcode == '10001':
+            #subtraction
+            summ = binToDec(RF[r1]) - binToDec(RF[r2])
+            if summ<0:
+                #overflow
+                temp = list(RF['111'])
+                temp[12] = 1
+                RF['111'] = ''.join(temp)
+            else:
+                RF[r3] = pcPrinter(0, 16)
+
+        if opcode == '10110':
+            #multiplication
+            summ = binToDec(RF[r1])*binToDec(RF[r2])
+            if len(pcPrinter(summ, 0))>16:
+                #overflow
+                temp = list(RF['111'])
+                temp[12] = 1
+                RF['111'] = ''.join(temp)
+            else:
+                RF[r3] = pcPrinter(summ, 16)
+        
+        if opcode == '11010':
+            #bitwise xor
+            RF[r3] = binToDec(RF[r1])^binToDec(RF[r2])
+        
+        if opcode == '11011':
+            #bitwise or
+            RF[r3] = binToDec(RF[r1])|binToDec(RF[r2])
+        
+        if opcode == '11100':
+            #bitwise and
+            RF[r3] = binToDec(RF[r1])&binToDec(RF[r2])
 
     if curType == 'B':
         r1 = instruction[5:8]
         imm = instruction[8:]
+
+        if opcode == '10010':
+            #mov immediate
+            
+
     
     if curType == 'C':
         r1 = instruction[10:13]
